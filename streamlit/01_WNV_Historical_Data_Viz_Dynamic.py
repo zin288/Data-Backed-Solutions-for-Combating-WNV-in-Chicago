698f99045ae4d7d5f9c6a9d8de5b6a79f91dce41
zin288 <jinjin28829@gmail.com>
2023-07-21 14:51:41 +0800

    Update 01_WNV_Historical_Data_Viz_Dynamic.py

--- streamlit/01_WNV_Historical_Data_Viz_Dynamic.py
+++ streamlit/01_WNV_Historical_Data_Viz_Dynamic.py
@@ -12,12 +12,13 @@ st.markdown("***Map showing weather stations locations, trap locations, number o
 # Load the train dataset
 @st.cache_data    
 def load_file(filepath):
-    current_directory = os.getcwd()
-    st.write("Current directory:", current_directory)
-    # file_path_concat = os.path.join(os.path.dirname(os.path.abspath(__file__)), filepath)
-    file_path_concat = os.path.join(current_directory, filepath)
-    train_df = pd.read_csv(file_path_concat)
+    # current_directory = os.getcwd()
+    # st.write("Current directory:", current_directory)
+    file_path_concat = os.path.join(os.path.dirname(os.path.abspath(__file__)), '/app/data-backed-solutions-for-combating-wnv-in-chicago')
     
+    # file_path_concat = os.path.join(current_directory, filepath)
+
+    file_path_concat = os.path.join(os.path.dirname(os.path.abspath(__file__)), filepath)
     pd.read_csv(filepath)
     return pd.read_csv(filepath)
 
